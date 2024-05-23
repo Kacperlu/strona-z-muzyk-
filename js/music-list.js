4b7aab63d8feab2e6b9761319f344ba26ccca15e
Kacperlu <127508179+Kacperlu@users.noreply.github.com>
2024-05-23 20:55:16 +0200

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "I was made for loving you",
+    artist: "Kiss",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Heavens's On Fire",
+    artist: "Kiss",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Rock and Roll All Nite",
+    artist: "Kiss",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Talking in your sleep",
+    artist: "The Romantics",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Somebody's whatching me",
+    artist: "Rockwell",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Beat It",
+    artist: "Micheal Jackson",
+    img: "music-6",
+    src: "music-6"
+  },
+ 
+];
